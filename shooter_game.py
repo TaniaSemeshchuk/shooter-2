0e79d637b9f81f13b12cf520b37af6a7626efe99
Tania Semeshchuk <74670974+TaniaSemeshchuk@users.noreply.github.com>
2024-04-07 15:16:23 +0300

    Update shooter_game.py
    Добавили пулі і зробили щоб вони стріляли

--- shooter_game.py
+++ shooter_game.py
@@ -10,14 +10,19 @@ fire_sound = mixer.Sound('fire.ogg')
 
 font.init()
 font1 = font.Font(None, 36)
+font2 = font.Font(None, 80)
+win = font2.render("You win!", True, (255, 255, 255))
+lose = font2.render("You lose!", True, (180, 0, 0))
 
 # нам потрібні такі картинки:
 img_back = "galaxy.jpg"  # фон гри
 img_hero = "rocket.png"  # герой
 img_enemy = "ufo.png"
+img_bullet = "bullet.png"
 
 score = 0
 lost = 0
+max_lost = 3
 
 # клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
@@ -57,7 +62,8 @@ class Player(GameSprite):
 
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
@@ -67,6 +73,12 @@ class Enemy(GameSprite):
             self.rect.x = randint(80, win_width-80)
             self.rect.y = 0
             lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 
 
@@ -85,6 +97,8 @@ for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width-80), -40, 80, 50, randint(1,5))
     monsters.add(monster)
 
+bullets = sprite.Group()
+
 
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
@@ -99,8 +113,10 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-
-
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
     if not finish:
         # оновлюємо фон
         window.blit(background, (0, 0))
@@ -113,11 +129,13 @@ while run:
         # рухи спрайтів
         ship.update()
         monsters.update()
+        bullets.update()
 
 
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
 
 
         display.update()
